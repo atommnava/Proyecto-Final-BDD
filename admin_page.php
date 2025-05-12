908e28a4bdda9511d510c9ac0aaf8cd5549fe2c3
atommnava <atommunoznava@gmail.com>
2025-05-12 14:08:19 -0600

    Backend hola

--- admin_page.php
+++ admin_page.php
@@ -18,9 +18,51 @@ if (!isset($_SESSION['email'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Admin</title>
     <link rel="stylesheet" href="style2.css">
+
+    <link rel="stylesheet" type="text/css" media="All" href="./css/ejemploAjaxStyle.css" />  
+	<link rel="shortcut icon" href="../img/iconos/favicon.ico">
+		
+	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+		
+	<script src="./js/ejemploAjaxScripts.js"></script>
 </head>
 
 <body style="background: #fff;">
+
+<div id="boton-redireccion">
+			<a href="index.php"  target="_self">
+				Login/Register
+			</a>
+		</div>
+
+		<div class="wrapContenido layout">
+			<div>
+				<nav>
+					<ul id="menu">
+						<li>
+							<a class= "comLink" href="javascript:;" onclick="despliegaContenido(1, 'ejemplo1', 1);"><span class="menu_home">CONTENIDO 1</span></a>
+							
+						</li>
+						<li>
+							<a class= "comLink" href="javascript:;" onclick="despliegaContenido(2, 'ejemplo2', 2);"><span class="menu_home">CONTENIDO 2</span></a>
+						</li>
+						<li>
+							<a class= "comLink" href="javascript:;" onclick="despliegaContenido(3, 'ejemplo3', 3);"><span class="menu_home">CONTENIDO 3</span></a>
+						</li>
+						<li>
+							<a class= "comLink" href="javascript:;" onclick="despliegaContenido(4, 'ejemplo4', 4);"><span class="menu_home">CONTENIDO 4</span></a>
+						</li>
+						
+					</ul>
+				</nav>
+			</div>
+	
+			<div id="contenido" class="contenido">
+				
+			</div>
+		</div>
+
     <div class="box">
         <h1>Bienvenido, <span><?= $_SESSION['name']; ?></span></h1>
         <p>Esta es la <span>página</span> Admin</p>
--- config.php
+++ config.php
@@ -4,4 +4,24 @@ $cfgServer['host'] = 'localhost';
 $cfgServer['user'] = '';
 $cfgServer['password'] = '';
 $cfgServer['dbname'] = '';
+/*
+ * @Author Atom Alexander M. Nava
+ * @brief Archivo de configuración para la conexión a la base de datos
+ *        Contiene las credenciales y establece la conexión.
+ * @date 
+ */
+
+ // Configuración a la base de datos.
+$host = "localhost";
+$user = "";
+$password = "";
+$database = "";
+
+// Crear conexión
+$conn = new mysqli($host, $user, $password, $database);
+
+// Verificar conexión
+if ($conn -> connect_error) {
+    die("Conn failed: ". $conn->connect_error);
+}
 ?>
